bf3cc93e5c0d6417a53de8bce8afcd763b2b912f
Chatchai Saratakij <CSaratakij@gmail.com>
2016-12-17 06:35:44 +0700

    Clean up config

--- st/config.h
+++ st/config.h
@@ -111,32 +111,6 @@ static const char *colorname[] = {
 	"#555555",
 };
 
-//	/* 8 normal colors */
-//	"black",
-//	"red3",
-//	"green3",
-//	"yellow3",
-//	"blue2",
-//	"magenta3",
-//	"cyan3",
-//	"gray90",
-//
-//	/* 8 bright colors */
-//	"gray50",
-//	"red",
-//	"green",
-//	"yellow",
-//	"#5c5cff",
-//	"magenta",
-//	"cyan",
-//	"white",
-//
-//	[255] = 0,
-//
-//	/* more colors can be added after 255 to use with DefaultXX */
-//	"#cccccc",
-//	"#555555",
-
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
